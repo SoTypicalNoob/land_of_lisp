3cfdef557ad39aefd2b8514873dedec803919272
SoTypicalNoob <so.typical.noob@gmail.com>
2022-09-23 18:07:43 +0200

    Base for guess my number and wizard game.

---
+++ the-wizards-adventure-game.lisp
@@ -0,0 +1,32 @@
+(defparameter *nodes* '((living-room (you are in the living-room.
+                                      a wizard is snoring loudly on the couch.))
+                        (garden (you are in a beautiful garden.
+                                 there is a wll in front of you))
+                        (attic (you are in the attic.
+                                there is a giant welding torch in the corner.))))
+(defparameter *objects* '(whiskey bucket frog chain))
+(defparameter *object-locations* '((whiskey living-room)
+                                   (bucket living-room)
+                                   (chain garden)
+                                   (frog garden)))
+
+(assoc 'garden *nodes*)
+
+(defun describe-location (location nodes)
+  (cadr (assoc location nodes)))
+
+(defparameter *edges* '((living-room (garden west door)
+                         (attic upstairs ladder))
+                        (garden (living-room east door))
+                        (attic (living-room downstairs ladder))))
+
+(defun describe-path (edge)
+  `(there is a ,(caddr edge) going ,(cadr edge) from here.))
+
+(defun describe-paths (location edges)
+  (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
+
+(defun objects-at (loc objs obj-locs)
+  (labels ((at-loc-p (obj)
+             (eq (cadr (assoc obj obj-locs)) loc)))
+    (remove-if-not #'at-loc-p objs)))
