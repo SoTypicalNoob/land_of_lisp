69e4da5a377eb6318eaed8aef4b0651203fc48e9
SoTypicalNoob <so.typical.noob@gmail.com>
2022-10-05 19:49:37 +0200

    random nodes/edges for GTW

--- grand-theft-wumpus.lisp
+++ grand-theft-wumpus.lisp
@@ -1,5 +1,6 @@
 (load "graph-util.lisp")
 
+;; Defining the Edges of Congestion City
 (defparameter *congestion-city-nodes* nil)
 (defparameter *congestion-city-edges* nil)
 (defparameter *visited-nodes* nil)
@@ -7,3 +8,51 @@
 (defparameter *edge-num* 45)
 (defparameter *worm-num* 3)
 (defparameter *cop-odds* 15)
+
+;; Generating Random Edges
+(defun random-node ()
+  (1+ (random *node-num*)))
+
+(defun edge-pair (a b)
+  (unless (eql a b)
+    (list (cons a b) (cons b a))))
+
+(defun make-edge-list ()
+  (apply #'append (loop repeat *edge-num*
+                        collect (edge-pair (random-node) (random-node)))))
+
+;; Preventing Islands
+(defun direct-edges (node edge-list)
+  (remove-if-not (lambda (x)
+                   (eql (car x) node))
+                 edge-list))
+
+(defun get-connected (node edge-list)
+  (let ((visited nil)
+        (labels ((traverse (node)
+                   (unless (member node visited)
+                     (push node visited)
+                     mapc (lambda (edge)
+                            (traverse (cdr edge)))
+                     (diret-edges node edge-list)))))
+        (traverse node))
+    visited))
+
+(defun find-islands (nodes edge-list)
+  (let ((islands nil))
+    (labels ((find-islands (nodes)
+               (let* ((connected (get-connected (car nodes) edge-list))
+                      (unconnected (set-difference nodes connected)))
+                 (push connected islands)
+                 (when unconnected
+                   (find-islands unconnected)))))
+      (find-islands nodes))
+    islands))
+
+(defun connect-with-bridges (islands)
+  (when (cdr islands)
+    (append (edge-pair (caar islands) (caadr islands))
+            (connect-with-bridges (cdr islands)))))
+
+(defun connect-all-islands (nodes edge-list)
+  (append (connect-with-bridges (find-islands nodes edge-list)) edge-list))
