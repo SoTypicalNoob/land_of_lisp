7da92d0d928c2057614722b525aa854f9e7eaf22
SoTypicalNoob <so.typical.noob@gmail.com>
2022-10-16 08:49:47 +0200

    Simulated Evolution

---
+++ simulated-evolution.lisp
@@ -0,0 +1,7 @@
+;; Using loop to Evolve!
+(defparameter *width* 100)
+(defparameter *height* 30)
+(defparameter *jungle* '(45 10 10 10))
+(defparameter *plant-energy* 80)
+
+;; Growing Plants in Our World
