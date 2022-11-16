434322efd76c25c8b01747d1017456e2eabe1e19
SoTypicalNoob <so.typical.noob@gmail.com>
2022-11-16 18:56:55 +0100

    Creating SVG-Specific Macros and Functions

--- svg.lisp
+++ svg.lisp
@@ -33,3 +33,36 @@
   (princ #\>))
 
 ;;; Creating the tag Macro
+(defmacro tag (name atts &body body)
+  `(progn (print-tag ',name
+                     (list ,@(mapcar (lambda (x)
+                                       `(cons ',(car x) ,(cdr x)))
+                                     (pairs atts)))
+                     nil)
+          ,@body
+          (print-tag ',name nil t)))
+
+;;; Creating SVG-Specific Macros and Functions
+(defmacro svg (&body body)
+  `(tag svg (xmlns "http://www.w3.org/2000/svg"
+                   "xmlns:xlink" "http://www.w3.org/1999/xlink")
+     ,@body))
+
+(defun brightness (col amt)
+  (mapcar (lambda (x)
+            (min 255 (max 0 (+ x amt))))
+          col))
+
+(defun svg-style (color)
+  (format nil
+          "~{fill:rgb(~a,~a,~a);stroke:rgb(~a,~a,~a)~}"
+          (append color
+                  (brightness color -100))))
+
+(defun circle (center radius color)
+  (tag circle (cx (car center)
+                  cy (cdr center)
+                  r radius
+                  style (svg-style color))))
+
+;;; Building a More Complicated SVG Example
