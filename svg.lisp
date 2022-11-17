c906f7cf1984c47aea6049673b7afbe46ff3d48a
SoTypicalNoob <so.typical.noob@gmail.com>
2022-11-17 19:23:54 +0100

    Random walk/svg file creation

--- svg.lisp
+++ svg.lisp
@@ -66,3 +66,30 @@
                   style (svg-style color))))
 
 ;;; Building a More Complicated SVG Example
+(defun polygon (points color)
+  (tag polygon (points (format nil
+                               "~{~a,~a ~}"
+                               (mapcan (lambda (tp)
+                                         (list (car tp) (cdr tp)))
+                                       points))
+                       style (svg-style color))))
+
+(defun random-walk (value length)
+  (unless (zerop length)
+    (cons value
+          (random-walk (if (zerop (random 2))
+                           (1- value)
+                           (1+ value))
+                  (1- length)))))
+
+(with-open-file (*standard-output* "random_walk.svg"
+                                   :direction :output
+                                   :if-exists :supersede)
+  (svg (loop repeat 10
+             do (polygon (append '((0 . 200))
+                                 (loop for x from 0
+                                       for y in (random-walk 100 400)
+                                       collect (cons x y))
+                                 '((400 . 200)))
+                         (loop repeat 3
+                               collect (random 256))))))
